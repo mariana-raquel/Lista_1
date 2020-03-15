891bfc137122a9cbacaa2fe15d3ae1d49c1a4d3e
Mariana Raquel <48134840+mariana-raquel@users.noreply.github.com>
2020-03-14 22:14:51 -0300

    Delete q3.cs

--- q3.cs
+++
@@ -1,7 +0,0 @@
-Console.WriteLine("Digite o raio de um círculo:");
-double raio = double.Parse(Console.ReadLine());
-double circ = 3.14 * 2 * raio;
-double area = 3.14 * (raio*raio);
-
-Console.WriteLine($"Circunferência = {circ}");
-Console.WriteLine($"Área = {area:0.00}");
